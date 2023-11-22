b0db4a1c94d79c96992af37868e9416a8615864b
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-11-22 13:38:24 +0100

    create application model

--- database/migrations/2023_11_22_102951_create_profiles_table.php
+++ database/migrations/2023_11_22_102951_create_profiles_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained();
+            $table->string('birthday');
+            $table->string('marital_status');
+            $table->string('gender');
+
             $table->timestamps();
         });
     }
