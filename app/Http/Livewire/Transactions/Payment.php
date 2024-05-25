a96fd68d12ce239c6933c1b59e5d7f0de5ffcf97
Bashar Umar <basharu83@gmail.com>
2024-05-25 23:30:24 +0100

    add computed properties

--- app/Http/Livewire/Transactions/Payment.php
+++ app/Http/Livewire/Transactions/Payment.php
@@ -3,9 +3,16 @@
 namespace App\Http\Livewire\Transactions;
 
 use Livewire\Component;
+use App\Models\Transaction;
+use Livewire\Attributes\Computed;
 
 class Payment extends Component
 {
+    #[Computed(persist: true)]
+    public function transactions()
+    {
+        return Transaction::where(['user_id' => auth()->id()])->get();
+    }
     public function render()
     {
         return view('livewire.transactions.payment');
