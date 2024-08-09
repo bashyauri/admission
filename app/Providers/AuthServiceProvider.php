55a1a4ab57023347e228e60fddfe6b243d5e685f
Bashar Umar <basharu83@gmail.com>
2024-08-09 12:54:57 +0100

    added policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Policies\UserPolicy;
+use App\Models\ProposedCourse;
 use Illuminate\Support\Facades\Gate;
+use App\Policies\ProposedCoursePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -17,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         User::class => UserPolicy::class,
+        ProposedCourse::class => ProposedCoursePolicy::class,
     ];
 
     /**
