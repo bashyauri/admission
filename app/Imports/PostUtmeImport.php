86bec9c9482c7498c89ee97dfc2b71f5c4f7b897
Bashar Umar <basharu83@gmail.com>
2026-07-27 14:43:24 +0100

    update postutmeimport to handle situation where a field is missing

--- app/Imports/PostUtmeImport.php
+++ app/Imports/PostUtmeImport.php
@@ -3,13 +3,13 @@
 namespace App\Imports;
 
 use App\Models\PostUtmeUpload;
+use App\Services\AcademicSessionService;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\ToModel;
-
-use App\Services\AcademicSessionService;
-use Maatwebsite\Excel\Concerns\WithUpserts;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithUpserts;
 
 class PostUtmeImport implements ToModel, WithHeadingRow, WithBatchInserts, WithUpserts
 {
@@ -23,13 +23,14 @@ class PostUtmeImport implements ToModel, WithHeadingRow, WithBatchInserts, WithU
     public function model(array $row)
     {
 
-        return new PostUtmeUpload([
-            'jamb_no' => $row['rg_num'],
-            'name'    => $row['rg_candname'],
-            'course' => $row['co_name'],
-            'jamb_score' => $row['rg_aggregate'],
-            'acad_session' => app(AcademicSessionService::class)->getAcademicSession(Auth::user())
-        ]);
+     return new PostUtmeUpload([
+    'jamb_no'      => Arr::get($row, 'rg_num'),
+    'name'         => Arr::get($row, 'rg_candname'),
+'course'       => Arr::get($row, 'co_name'),
+    'jamb_score'   => Arr::get($row, 'rg_aggregate'),
+    'acad_session' => app(AcademicSessionService::class)
+        ->getAcademicSession(Auth::user()),
+]);
     }
 
     /**
